3a8b09db9c080bb7a22f95e3e3dbd28a185c93ff
Andrey Bychenkow <decebell032@yandex.ru>
2024-03-23 11:24:42 +0300

    the function is written

--- Module14/03_least_divisor/main.py
+++ Module14/03_least_divisor/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def smallest_divisor(n):
+    for divisor in range(2, n):
+        if n % divisor == 0:
+            return divisor
+
+    return n
+
+
+while True:
+    num = int(input("Введите число: "))
+    smallest_div = smallest_divisor(num)
+    print("Наименьший делитель, отличный от единицы:", smallest_div, "\n")
