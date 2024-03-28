6fbd9d0a1ef2c0cca4108d8df2d400d31efeaf74
Andrey Bychenkow <decebell032@yandex.ru>
2024-03-28 11:21:59 +0300

    the code is written

--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+participants = ['Артемий', 'Борис', 'Влад', 'Гоша', 'Дима', 'Евгений', 'Женя', 'Захар']
+
+first_day_participants = []
+
+for i in range(0, len(participants), 2):
+    first_day_participants.append(participants[i])
+
+print("Первый день:", first_day_participants)
