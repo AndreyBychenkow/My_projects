2ac0424bd0da5e109ea06b8ee8827531b001a3c2
Andrey Bychenkow <decebell032@yandex.ru>
2024-03-28 11:07:41 +0300

    the code is written

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -6,4 +6,4 @@ while True:
     for i in range(1, N + 1, 2):
         numbers.append(i)
 
-    print("Список нечетных чисел от 1 до", N, ":", numbers, "\n")
+    print("Список нечетных чисел от 1 до", N , ":", numbers, "\n")
