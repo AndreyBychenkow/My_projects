c54d19cc4a1186d1cd598b7658f550fc870971eb
Andrey Bychenkow <decebell032@yandex.ru>
2024-03-28 11:04:28 +0300

    the code is written

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+while True:
+
+    N = int(input("Введите целое число N: "))
+    numbers = []
+
+    for i in range(1, N + 1, 2):
+        numbers.append(i)
+
+    print("Список нечетных чисел от 1 до", N, ":", numbers, "\n")
