4bf80caf7c2a68b561d464d2433bb9786ddd5efc
Andrey Bychenkow <decebell032@yandex.ru>
2024-04-18 19:13:21 +0300

    no comments

--- Module20/05_sort_function/main.py
+++ Module20/05_sort_function/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def tpl_sort(tpl):
+    if all(isinstance(x, int) for x in tpl):
+        return tuple(sorted(tpl))
+    else:
+        return tpl
+
+# tpl = (6, "a", -1, 8, 4, 10, -5)
+# print(tpl_sort(tpl))
+
+# На просторах интернета нашел вот такую интересную встроенную функцию
+# isinstance(x, int) , которая проверяет, является ли объект x указанного класса
+# (в нашем случае int). Если x является целым числом, функция возвращает True, иначе False.
